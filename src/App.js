a1d82a2b07dee17a7e60e6c8b966d780c0db7501
Dana Simmons <dana@simmons-audio.com>
2019-04-11 18:09:23 -0400

    login works with a router link

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
+import GameContainer from './components/GameContainer';
 
 import { Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from "history";
+
 
 
 class App extends Component {
 
-  state = {
-    users: []
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      user: null
+    }
   }
-
+  customHistory = createBrowserHistory()
+  
+  setUser = (payload) => {
+    console.log('user', payload)
+    localStorage.setItem('token', payload.token)
+    
+    this.setState({user:payload.user},()=>this.customHistory.push("/test"))
+    this.forceUpdate()
+  }
+  
 
   render() {
     return (
       <div className="App">
       <Switch>
-        < Route path="/signup" component={SignUp} />
+        < Route path="/signup" render={()=><SignUp setUser={this.setUser}/>  } />
+        < Route path="/home" component={GameContainer}/>
+        < Route path="/" component={GameContainer} />
       </Switch>
       </div>
     );
