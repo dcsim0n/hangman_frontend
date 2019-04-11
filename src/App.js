fbd454369271db397faadc20573d4048217fd143
Dana Simmons <dana@simmons-audio.com>
2019-04-11 18:25:10 -0400

    login redirect is working!!!! PRAISE JESUS!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
 import GameContainer from './components/GameContainer';
 
 import { Route, Switch } from 'react-router-dom'
-import { createBrowserHistory } from "history";
+//import { createBrowserHistory } from "history";
 
 
 
@@ -18,14 +18,14 @@ class App extends Component {
       user: null
     }
   }
-  customHistory = createBrowserHistory()
+  //customHistory = createBrowserHistory()
   
   setUser = (payload) => {
     console.log('user', payload)
     localStorage.setItem('token', payload.token)
     
-    this.setState({user:payload.user},()=>this.customHistory.push("/test"))
-    this.forceUpdate()
+    this.setState({user: payload.user})
+    //this.forceUpdate()
   }
   
 
@@ -34,7 +34,6 @@ class App extends Component {
       <div className="App">
       <Switch>
         < Route path="/signup" render={()=><SignUp setUser={this.setUser}/>  } />
-        < Route path="/home" component={GameContainer}/>
         < Route path="/" component={GameContainer} />
       </Switch>
       </div>
--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class GameContainer extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         guesses: 0,
+         letters: [ ],
+         started: false
+      }
+    }
+    
   render() {
     return (
       <div>
         Hello World
+        
       </div>
     )
   }
