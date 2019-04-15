74fb8f8e23820a612ff325dfff4837bae1b92658
Dana Simmons <dana@simmons-audio.com>
2019-04-15 10:59:42 -0400

    this looks better, we won't render an input field with spaces

--- src/components/LetterBox.js
+++ src/components/LetterBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LetterBox(props) {
   return (
     <div className="column ui massive center input" >
-      <input type="text" onChange={console.log} value={props.value} disabled={props.value === " "? "disabled": ""}/>
+      {props.value === " "? null : <input type="text" onChange={console.log} value={props.value}/> }
     </div>
   )
 }
