796e4e85780ef99dbf8b43003f3ce37da54a4f5a
Dana Simmons <dana@simmons-audio.com>
2019-04-16 14:28:56 -0400

    game over animation spinns

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Button, Header, Image, Modal} from 'semantic-ui-react'
 import FaceImg from '../assets/face.png'
+import './SpinAnimation.css'
 
 export default function GameOverModal(props) {
     
@@ -8,7 +9,7 @@ export default function GameOverModal(props) {
       <Modal open={props.modalOpen} >
         <Modal.Header>{props.won? "CONGRATULATIONS, YOU WON!" : "YOU LOST!"}</Modal.Header>
             <Modal.Content image>
-                <Image size='medium' src={FaceImg} />
+                <Image src={FaceImg} alt="spinning face" className="spinning-logo"/>
                 <Modal.Description>
                     <Header>Your Score: {props.score}</Header>
                     <p>The Word: {props.word}</p>
