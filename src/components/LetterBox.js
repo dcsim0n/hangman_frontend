84d8a1addf75142cadc63a0da4dac3f7018917b9
Dana Simmons <dana@simmons-audio.com>
2019-04-17 14:45:41 -0400

    reformated text inputs and limit word lenght to 16 characters

--- src/components/LetterBox.js
+++ src/components/LetterBox.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function LetterBox(props) {
   return (
-    <div className="column ui massive center input" >
-      {props.value === " "? null : <input type="text" onKeyPress={props.handleGuess} value={props.value}/> }
+    <div className="column" >
+      {props.value === " "? null :<input style={{width:".8em" ,fontSize:'4em'}} type="text" onKeyPress={props.handleGuess} value={props.value}/>}
     </div>
   )
 }
