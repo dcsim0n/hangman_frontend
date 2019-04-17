e48de89b749b2bc78683838ea46f142f1173ef62
Leigh Baugham <leigh.baugham@gmail.com>
2019-04-17 14:13:47 -0400

    fixing update leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,31 +3,13 @@ import { Image, List } from 'semantic-ui-react'
 
 export default class LeaderBoard extends Component {
 
-    constructor(props) {
-        super(props)
-    
-
-    this.state = {
-        leaders: []
-        }
-    }
-
-componentDidMount(){
-    fetch('http://localhost:3000/leaders')
-    .then(res => res.json())
-    .then(data => {
-        this.setState({leaders: data})
-        console.log(this.state.leaders)      
-    })
-  }
-
 
   render() {
       return (
           <div>
                <List celled='true' animated verticalAlign='middle' >
                <h2>Leader Board </h2>
-               {this.state.leaders.map(leader => <List.Item content={`${leader.name} : ${leader.total_score}`} />)}               
+               {this.props.leaders.map(leader => <List.Item content={`${leader.name} : ${leader.total_score}`} />)}               
                </List>
           </div>
       )
